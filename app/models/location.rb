95e2a611318c47928b5899d30d0e96248365d670
djlinkstrom <djlinkstrom@gmail.com>
2014-06-15 20:30:52 -0400

    businesses styling business pages

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,6 +1,6 @@
 class Location < ActiveRecord::Base
 	belongs_to :user
 	has_many :events
-	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "80x80>" }, :default_url => "/images/best_time_logo_text_only.png"
+	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "80x80>" }, :default_url => "/images/best_time_logo_very_small.png"
 	
 end
