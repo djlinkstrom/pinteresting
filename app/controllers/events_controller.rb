7d01a29e445155a6076958406c3701765c7d84cd
djlinkstrom <djlinkstrom@gmail.com>
2014-06-19 22:56:52 -0400

    logic to identify edit event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -48,7 +48,7 @@ class EventsController < ApplicationController
   def update
     respond_to do |format|
       if @event.update(event_params)
-        format.html { redirect_to @event, notice: 'Event was successfully updated.' }
+        format.html { redirect_to [@location, @event], notice: 'Event was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: 'edit' }
--- app/models/event.rb
+++ app/models/event.rb
@@ -2,16 +2,5 @@ class Event < ActiveRecord::Base
 	belongs_to :location
 	has_attached_file :image, :styles => { :medium => "300x300#", :thumb => "120x120#" }, :default_url => "/images/best-time-default.png"
 
-	def eventstart_date_field  # What this returns will be what is shown in the field
-	  full_date.strftime("%m-%d'%y") if full_date.present?
-	end 
-
-	def eventstart_time_field
-	  full_date.strftime("%I:%M%p") if full_date.present?
-	end
-
-	def time_field=(time)
-	  eventstart = DateTime.parse("#{eventstart_date_field} #{eventstart_time_field}")
-	end
-
+	
 end
